ea96c144789d3c5937bf6e739d9588c1f4f0a366
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-05-16 22:27:56 +0200

    Http request

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
   term = '';
 
   constructor() { }
@@ -15,7 +16,7 @@ export class SearchBarComponent implements OnInit {
 
   onFormSubmit(event : any) {
     event.preventDefault();
-    console.log(this.term)
+    this.submitted.emit(this.term);
   }
   
 
